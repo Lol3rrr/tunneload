828e2ce6190d52238e4a22395ca7e39a1137193a
lol3rrr <s.loler03@gmail.com>
2021-02-25 18:59:48 +0100

    Added the first very crude tls implementation for the Reader/Sender traits

--- src/tls/receiver.rs
+++ src/tls/receiver.rs
@@ -1,20 +1,26 @@
 use crate::acceptors::traits::Receiver as ReceiverTrait;
 
 use async_trait::async_trait;
+use rustls::Session;
+use std::io::Read;
 
 pub struct Receiver<'a, R>
 where
     R: ReceiverTrait + Send,
 {
     og_read: &'a mut R,
+    session: std::sync::Mutex<rustls::ServerSession>,
 }
 
 impl<'a, R> Receiver<'a, R>
 where
     R: ReceiverTrait + Send,
 {
-    pub fn new(og: &'a mut R) -> Self {
-        Self { og_read: og }
+    pub fn new(og: &'a mut R, session: std::sync::Mutex<rustls::ServerSession>) -> Self {
+        Self {
+            og_read: og,
+            session,
+        }
     }
 }
 
@@ -24,6 +30,14 @@ where
     R: ReceiverTrait + Send,
 {
     async fn read(&mut self, buf: &mut [u8]) -> std::io::Result<usize> {
-        self.og_read.read(buf).await
+        let mut tmp: Vec<u8> = Vec::with_capacity(buf.len());
+        self.og_read.read(&mut tmp).await.unwrap();
+
+        let mut tls_session = self.session.lock().unwrap();
+
+        let mut tmp_buffer = std::io::Cursor::new(tmp);
+        tls_session.read_tls(&mut tmp_buffer).unwrap();
+        tls_session.process_new_packets().unwrap();
+        tls_session.read(buf)
     }
 }
--- src/tls/sender.rs
+++ src/tls/sender.rs
@@ -1,29 +1,49 @@
 use crate::acceptors::traits::Sender as SenderTrait;
 
 use async_trait::async_trait;
+use rustls::Session;
+use std::io::Write;
 
-pub struct Sender<'a, S>
+pub struct Sender<'a, 'b, S>
 where
     S: SenderTrait + Send,
 {
     og_send: &'a mut S,
+    session: &'b std::sync::Mutex<rustls::ServerSession>,
 }
 
-impl<'a, S> Sender<'a, S>
+impl<'a, 'b, S> Sender<'a, 'b, S>
 where
     S: SenderTrait + Send,
 {
-    pub fn new(og: &'a mut S) -> Self {
-        Self { og_send: og }
+    pub fn new(og: &'a mut S, session: &'b mut std::sync::Mutex<rustls::ServerSession>) -> Self {
+        Self {
+            og_send: og,
+            session,
+        }
+    }
+
+    fn write_tls(&self, buf: Vec<u8>) -> Vec<u8> {
+        let mut s_writer = self.session.lock().unwrap();
+
+        let written = s_writer.write(&buf).unwrap();
+        let mut result: Vec<u8> = Vec::with_capacity(written);
+
+        s_writer.write_tls(&mut result).unwrap();
+
+        result
     }
 }
 
 #[async_trait]
-impl<'a, S> SenderTrait for Sender<'a, S>
+impl<'a, 'b, S> SenderTrait for Sender<'a, 'b, S>
 where
     S: SenderTrait + Send,
 {
-    async fn send(&mut self, buf: Vec<u8>, length: usize) {
-        self.og_send.send(buf, length).await
+    async fn send(&mut self, buf: Vec<u8>, _length: usize) {
+        let send_data = self.write_tls(buf);
+
+        let send_length = send_data.len();
+        self.og_send.send(send_data, send_length).await
     }
 }
