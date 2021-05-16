f84a672243a8ec495c5e9637fee379b093e1d292
lol3rrr <s.loler03@gmail.com>
2021-05-16 17:45:43 +0200

    Fixed check for websocket

--- src/websockets/is_websocket.rs
+++ src/websockets/is_websocket.rs
@@ -2,7 +2,7 @@ use stream_httparse::Request;
 
 pub fn is_websocket(req: &Request) -> bool {
     match req.headers().get("Upgrade") {
-        Some(val) if val.to_string().to_lowercase() != "websocket" => true,
+        Some(val) if val.to_string().to_lowercase() == "websocket" => true,
         _ => false,
     }
 }
