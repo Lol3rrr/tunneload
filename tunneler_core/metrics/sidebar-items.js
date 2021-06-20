378bbfe745f128a1b331c05cbb74200a5689448b
Lol3rrr <Leon@lol3r.net>
2021-06-20 11:19:11 +0000

    Deploying to gh-pages from @ Lol3rrr/tunneload@f1b2f33ab6b3321c808adb33923f5a1f639ff9e5 🚀

--- tunneler_core/metrics/sidebar-items.js
+++ tunneler_core/metrics/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"struct":[["Empty","An empty Metrics-Collector that does not keep track of any metrics it receives and simply discards them."]]});
+initSidebarItems({"struct":[["Empty","An empty Metrics-Collector that does not keep track of any metrics it receives and simply discards them."]],"trait":[["Metrics","The Interface used to collect metrics from the Tunneler-Software. This allows for the usage of a variety of different Metrics-Systems as long as they can provide an implementation of this Interface."]]});
