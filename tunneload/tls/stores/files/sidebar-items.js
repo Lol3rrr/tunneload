fd898c979a30b09becaa1eb110cea263a7d92398
Lol3rrr <Leon@lol3r.net>
2021-07-07 16:36:54 +0000

    Deploying to gh-pages from @ Lol3rrr/tunneload@b684981e600bd213457bf173cfca005ec1edb6b5 🚀

---
+++ tunneload/tls/stores/files/sidebar-items.js
@@ -0,0 +1,1 @@
+initSidebarItems({"struct":[["FileStore","This is the main Datastructure for handling generated Certificates in File-Format"]]});
--- tunneload/tls/stores/sidebar-items.js
+++ tunneload/tls/stores/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"mod":[["kubernetes","This module contains all the TLS-relevant Storage stuff for Kubernetes"]]});
+initSidebarItems({"mod":[["files","This modules provides the File-Storage for Certificates"],["kubernetes","This module contains all the TLS-relevant Storage stuff for Kubernetes"]]});
