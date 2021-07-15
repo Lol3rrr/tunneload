dc5d225330f3f5fe9ba30c382f95a5c830aaef4e
Lol3rrr <Leon@lol3r.net>
2021-07-15 20:23:22 +0000

    Deploying to gh-pages from @ Lol3rrr/tunneload@97dc03b0fb3ad11dbfb57375a5a3c47c657fd167 🚀

--- tunneload/rules/parser/sidebar-items.js
+++ tunneload/rules/parser/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"fn":[["parse_matchers","Parses a raw String that defines matchers"]]});
+initSidebarItems({"enum":[["ParseMatcherError","The Error returned when Parsing Matchers"]],"fn":[["parse_matchers","Parses a raw String that defines matchers"]]});
