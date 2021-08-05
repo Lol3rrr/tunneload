77f66654ced6edd1e65179219e4dd908c03dfd61
Lol3rrr <Leon@lol3r.net>
2021-08-05 13:29:34 +0000

    Deploying to gh-pages from @ Lol3rrr/tunneload@a863b2c11d9636da0fea4d813a6c3238e790f3e8 🚀

--- tunneload/configurator/kubernetes/traefik_bindings/middleware/sidebar-items.js
+++ tunneload/configurator/kubernetes/traefik_bindings/middleware/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"struct":[["Middleware","Auto-generated derived type for MiddlewareSpec via `CustomResource`"],["MiddlewareSpec","The Spec for Traefik based Middleware ressources"]],"type":[["Config","The underlying Config for Middlewares"]]});
+initSidebarItems({"struct":[["BasicAuth",""],["Compress",""],["Middleware","Auto-generated derived type for MiddlewareSpec via `CustomResource`"],["MiddlewareSpec","The Spec for Traefik based Middleware ressources"],["StripPrefix",""]]});
