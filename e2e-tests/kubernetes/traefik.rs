e1a500c31dca10d91476a57b403a9180c1d0be92
lol3rrr <s.loler03@gmail.com>
2021-08-06 23:56:43 +0200

    Added more to the E2E-Tests
    Added some more parts to the E2E Tests, more specifically I am now checking all the currently available
    middlewares and if their Traefik-Configurations are parsed and handled correctly, as expected

--- e2e-tests/kubernetes/traefik.rs
+++ e2e-tests/kubernetes/traefik.rs
@@ -15,13 +15,54 @@ pub async fn load_middleware() {
 
     let middlewares = g_conf.load_middlewares(&plugin_list).await;
 
-    let loaded_middleware = middlewares
-        .into_iter()
+    let strip_prefix_middleware = middlewares
+        .iter()
         .find(|m| m.get_name() == "testing-middleware-strip-prefix")
         .expect("The Middleware should have been loaded");
-
-    match loaded_middleware.get_action() {
+    match strip_prefix_middleware.get_action() {
         tunneload::rules::Action::RemovePrefix(prefix) if prefix == "/test" => {}
+        _ => assert!(false),
+    };
+
+    let headers_middleware = middlewares
+        .iter()
+        .find(|m| m.get_name() == "testing-middleware-headers")
+        .expect("The Middleware should have been loaded");
+    match headers_middleware.get_action() {
+        tunneload::rules::Action::AddHeaders(headers) => assert_eq!(
+            &vec![
+                ("test-header".to_owned(), "test-value".to_owned()),
+                ("other-header".to_owned(), "other-value".to_owned())
+            ],
+            headers
+        ),
+        _ => assert!(false),
+    };
+
+    let headers_cors_middleware = middlewares
+        .iter()
+        .find(|m| m.get_name() == "testing-middleware-headers-cors")
+        .expect("The Middleware should have been loaded");
+    match headers_cors_middleware.get_action() {
+        tunneload::rules::Action::Cors(opts) => assert_eq!(
+            &tunneload::rules::CorsOpts {
+                origins: vec![],
+                max_age: None,
+                credentials: false,
+                methods: vec!["GET".to_owned()],
+                headers: vec![],
+            },
+            opts
+        ),
+        _ => assert!(false),
+    };
+
+    let compress_middleware = middlewares
+        .iter()
+        .find(|m| m.get_name() == "testing-middleware-compress")
+        .expect("The Middleware should have been loaded");
+    match compress_middleware.get_action() {
+        tunneload::rules::Action::Compress => {}
         _ => assert!(false),
     };
 }
